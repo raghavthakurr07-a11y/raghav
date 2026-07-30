5a9321196ae84c6168e4c76999459913370ff904
Raghav Thakur <raghavthakurr07@gmail.com>
2026-07-30 20:56:38 +0530

    Fix indentation and formatting in multiplication table output

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
-"""num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
 for i in range(1, 11):
-    print(num, "x", i, "=", num * i)"""
+    print(num, "x", i, "=", num * i)
     
 num = int(input("Enter a number: "))
 for i in range(1, 11):
