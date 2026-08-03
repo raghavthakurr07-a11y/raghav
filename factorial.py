4a45e1ce756ec9b9a324ae7d5f82abc2b3d1d903
Raghav Thakur <raghavthakurr07@gmail.com>
2026-08-03 11:06:41 +0530

    Refactor code structure and fix formatting in factorial.py

--- factorial.py
+++ factorial.py
@@ -1,24 +1,31 @@
 #find factorial of a number
+
 """num=int(input("Enter a number: "))
 factorial=1
 for i in range(1, num + 1):
     factorial *= i
 print("The factorial of", num, "is", factorial)"""
+
 #reverse string using reversed function
+
 """text=input("enter a string: ")
 for i in reversed(text):
     print(i)"""
+    
     # reverse string using for loop
+    
 """text=input("enter a string: ")
 reversed_text=""
 for i in text:
         reversed_text=i+reversed_text
 print(reversed_text)"""
+
 #find no of vowels in a string
-text=input("enter a string: ")
+
+"""text=input("enter a string: ")
 vowels="aeiouAEIOU"
 count=0
 for i in text:
     if i in vowels:
         count += 1
-print("The number of vowels in the string is:", count)
+print("The number of vowels in the string is:", count)"""
