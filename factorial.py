0046005c7495ce236fd0786c2bb73612d61daea8
Raghav Thakur <raghavthakurr07@gmail.com>
2026-08-03 10:53:58 +0530

    Fix string reversal implementation and update input handling

--- factorial.py
+++ factorial.py
@@ -1,9 +1,24 @@
+#find factorial of a number
 """num=int(input("Enter a number: "))
 factorial=1
 for i in range(1, num + 1):
     factorial *= i
 print("The factorial of", num, "is", factorial)"""
-
-text=input("enter a string: ")
+#reverse string using reversed function
+"""text=input("enter a string: ")
 for i in reversed(text):
-    print(i)
+    print(i)"""
+    # reverse string using for loop
+"""text=input("enter a string: ")
+reversed_text=""
+for i in text:
+        reversed_text=i+reversed_text
+print(reversed_text)"""
+#find no of vowels in a string
+text=input("enter a string: ")
+vowels="aeiouAEIOU"
+count=0
+for i in text:
+    if i in vowels:
+        count += 1
+print("The number of vowels in the string is:", count)
