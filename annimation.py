e0f2003a4149be108c040a7eb9339804f3b366c6
Raghav Thakur <raghavthakurr07@gmail.com>
2026-08-11 20:34:24 +0530

    Refactor HTML structure in 1lab.html, add birthday celebration page, and implement prime number checking function in class room work.py

--- annimation.py
+++ annimation.py
@@ -122,6 +122,7 @@ import os
 message = """
 
 
+
     
     
     
--- class room work.py
+++ class room work.py
@@ -149,3 +149,19 @@ else:
     for j in range(i, 0, -1):
         print(j, end=" ")
     print()"""
+    
+#check prime or not using function
+
+"""def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(num, "is a prime number.")
+else:
+    print(num, "is not a prime number.")"""
